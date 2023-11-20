d542a0b89ff199c09ef7f9dc16b96e445710165d
palakagr34 <palakagr34@gmail.com>
2023-11-19 22:25:43 -0500

    Part 2

--- trackTrajectory.py
+++ trackTrajectory.py
@@ -13,6 +13,8 @@ kf = KalmanFilter(dim_x=4, dim_z=2)
 # Set initial state 
 detections_folder = "./detections"
 files = os.listdir(detections_folder)
+
+
 image_path = os.path.join(detections_folder, files[0])
 
 first_image = cv2.imread(image_path)
@@ -41,52 +43,75 @@ kf.Q = Q_discrete_white_noise(dim=4, dt=0.1, var=0.13)
 
 # Path to detections folder
 detections_folder = "./detections"
+output_folder = "./tracking_videos"
+if not os.path.exists(output_folder): 
+    os.makedirs(output_folder)
 
-output_video_path = "path_to_output_video.mp4"
+frame_numbers = [int(file.split("_")[2].split(".")[0]) for file in files if "frame_" in file]
+frame_numbers.sort()
+# Find the continuous ranges in the sequence
+continuous_ranges = []
+start = None
+for i in range(frame_numbers[0], frame_numbers[-1]):
+    if i in frame_numbers:
+        if start is None:
+            start = i
+    elif start is not None:
+        continuous_ranges.append((start, i - 1))
+        start = None
+
+continuous_ranges.append((start, frame_numbers[-1]))  # To capture the last segment
+
+
 frame_width = width  
 frame_height = height  
 print("width: ", width)
 print("height: ", height)
-output_video = cv2.VideoWriter(output_video_path,
-                               cv2.VideoWriter_fourcc(*'mp4v'),
-                               30, (frame_width, frame_height))
+
 
 run = 0
 # Loop through the detections folder
-for filename in os.listdir(detections_folder):
-    run+=1
-    print(f"Run: {run}")
-    if filename.endswith(".jpg"):
-        frame = cv2.imread(os.path.join(detections_folder, filename))
+for start, end in continuous_ranges:
+    output_video_path = f"{output_folder}/tracking_{start}_{end}.mp4"
+    print(f"Creating video: {output_video_path}")
+
+    output_video = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc(*'mp4v'), 30, (frame_width, frame_height))
+    for frame_number in range(start, end + 1):
+        filename = f"frame_{frame_number}.jpg"
+
+        if filename in files:
+            frame = cv2.imread(os.path.join(detections_folder, filename))
         
-        detected_boxes = detect_drones(frame)
-        print(f"Detected boxes: {detected_boxes}")
+            detected_boxes = detect_drones(frame)
+            print(f"Detected boxes: {detected_boxes}")
 
 
-        if len(detected_boxes) > 0:
-            max_confidence_idx = np.argmax(detected_boxes[:, -1])  # Assuming last column is the confidence score
-            chosen_box = detected_boxes[max_confidence_idx]
+            if len(detected_boxes) > 0:
+                max_confidence_idx = np.argmax(detected_boxes[:, -1])  # Assuming last column is the confidence score
+                chosen_box = detected_boxes[max_confidence_idx]
 
-            x1, y1, x2, y2 = chosen_box  # Assuming the box is in format [x1, y1, x2, y2]
-            measurement = [(x1 + x2) / 2, (y1 + y2) / 2]  # Using center of the chosen box as measurement
+                x1, y1, x2, y2 = chosen_box  # Assuming the box is in format [x1, y1, x2, y2]
+                measurement = [(x1 + x2) / 2, (y1 + y2) / 2]  # Using center of the chosen box as measurement
 
 
-            prediction = kf.predict()
-            print(f"Prediction: {prediction}")  
+                prediction = kf.predict()
+                print(f"Prediction: {prediction}")  
 
-            if prediction is not None: 
-                predicted_x, predicted_y = prediction[:2]
+                if prediction is not None: 
+                    predicted_x, predicted_y = prediction[:2]
 
-                kf.update(measurement)
+                    kf.update(measurement)
 
-                frame = cv2.circle(frame, (int(predicted_x), int(predicted_y)), 5, (0, 255, 0), -1)
+                    frame = cv2.circle(frame, (int(predicted_x), int(predicted_y)), 5, (0, 255, 0), -1)
 
-                if prev_detection_x is not None and prev_detection_y is not None:
-                    frame = cv2.line(frame, (int(prev_detection_x), int(prev_detection_y)),
-                                (int(predicted_x), int(predicted_y)), (0, 255, 0), 2)
+                    if prev_detection_x is not None and prev_detection_y is not None:
+                        frame = cv2.line(frame, (int(prev_detection_x), int(prev_detection_y)),
+                                    (int(predicted_x), int(predicted_y)), (0, 255, 0), 2)
 
-                prev_detection_x, prev_detection_y = predicted_x, predicted_y
+                    prev_detection_x, prev_detection_y = predicted_x, predicted_y
 
-        output_video.write(frame)
-        
+                output_video.write(frame)
+            else:
+                print(f"Frame {frame_number} is missing")
+
 output_video.release()
